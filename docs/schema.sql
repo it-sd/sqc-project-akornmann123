f560663e6e8831c732ead94d9aaabf9dc33aadb0
akornmann123 <123998259+akornmann123@users.noreply.github.com>
2023-02-21 21:03:39 -0600

    Create schema.sql

---
+++ docs/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS Species, Individual Species, Country, Threats, History, Habitat;
+
+CREATE TABLE Species(
+  Species_Id string
+)
+CREATE TABLE Individual_Species(
+  Species_Id string,
+  Species_Name string
+)
+CREATE TABLE Country(
+  Species_Id string,
+  Country string,
+  Region string
+)
+CREATE TABLE Threats(
+  Species_Id string
+  Species_Name string
+)
+CREATE TABLE Habitats(
+  Species_Id string
+  Species_Name string
+)
+CREATE TABLE History(
+  Species_Id string
+  Species_Name string
+)
