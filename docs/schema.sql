450fd005d3de42540f3136090cfbe49bbe99d30d
akornmann123 <123998259+akornmann123@users.noreply.github.com>
2023-02-26 19:43:50 -0600

    Adding semicolons

--- docs/schema.sql
+++ docs/schema.sql
@@ -2,25 +2,31 @@ DROP TABLE IF EXISTS Species, Individual Species, Country, Threats, History, Hab
 
 CREATE TABLE Species(
   Species_Id string
-)
+);
+
 CREATE TABLE Individual_Species(
   Species_Id string,
   Species_Name string
-)
+);
+
 CREATE TABLE Country(
   Species_Id string,
   Country string,
   Region string
-)
+);
+
 CREATE TABLE Threats(
   Species_Id string
   Species_Name string
-)
+);
+
 CREATE TABLE Habitats(
   Species_Id string
   Species_Name string
-)
+);
+
 CREATE TABLE History(
   Species_Id string
   Species_Name string
-)
+);
+
